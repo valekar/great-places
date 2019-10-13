f6243d7f70207a55b0d79254581e8b179caa446b
valekar <srini@DESKTOP-1FN9MGD.localdomain>
2019-10-13 22:02:48 +0530

    great places

--- store/actions/places.js
+++ store/actions/places.js
@@ -5,8 +5,21 @@ export const CREATE_PLACE = "CREATE_PLACE";
 
 export const createPlace = place => {
   return async dispatch => {
-    const fileName = place.imageUrl.split("/").pop();
+    const response = await fetch(
+      `https://maps.googleapis.com/maps/api/geocode/json?latlng${place.address.line1},${place.address.line2}&key=YOUR_API_KEY&callback=initMap`
+    );
 
+    if (!response.ok) {
+      throw new Error("");
+    }
+
+    const resData = await response.json();
+
+    if (!resData.results) {
+      throw new Error("Something went wrong");
+    }
+
+    const fileName = place.imageUrl.split("/").pop();
     const newPath = FileSystem.documentDirectory + fileName;
     try {
       await FileSystem.moveAsync({
